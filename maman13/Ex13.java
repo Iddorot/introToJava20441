35e61788a370be5a0ee7390c5719c91f2411f63b
Ido Rot <idorot9768@gmail.com>
2023-05-27 17:51:56 +0300

    psudo code q1

--- maman13/Ex13.java
+++ maman13/Ex13.java
@@ -1,7 +1,13 @@
+public class Ex13 {
+    public static int shortestRoad(int[] road1, int[] road2) {
+        return shortestRoad(road1, road2, road1.length + 1, road2.length + 1);
+    }
 
-public class Ex13
-{
-    public static int shortestRoad (int [] road1, int [] road2){
-        
+    private static int shortestRoad(int[] road1, int[] road2, int road1Length, int road2Length) {
+        int[][] dp = new int[road1Length][road2Length];
+
+        // logic for calculating the smallest sum
+
+        return dp[road1Length - 1][road2Length - 1];
     }
 }
